cd8bc3975c7797132a581ddcb26b71cadbe8206a
margaiwangara <margaiwangara@gmail.com>
2019-11-06 15:21:37 +0300

    added error handler to display pretty error messages to the user

--- index.js
+++ index.js
@@ -10,15 +10,18 @@ const app = express();
 
 // invoke middlewares
 app.use(cors());
+
+// Error Handler
 app.use(function(req, res, next) {
   let error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
+const errorHandler = require("./handlers/errors");
+app.use(errorHandler);
 
+// set PORT and run app
 const PORT = process.env.PORT || 5000;
-
-// Run app
 app.listen(
   PORT,
   console.log(`App running in ${process.env.NODE_ENV} mode on port ${PORT}`)
