f428b609318f1a7a4dc4c27cddf932c2cbea4a88
margaiwangara <margaiwangara@gmail.com>
2019-11-26 12:30:15 +0300

    enabled cors for all routes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const helmet = require("helmet");
 const xssClean = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
+const cors = require("cors");
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -37,6 +38,7 @@ app.use(helmet()); //helmet to add headers and prevent security flaws
 app.use(xssClean()); //prevent xss attacks eg <script></script> tags in db
 app.use(limiter); //no of request rate limited
 app.use(hpp()); //prevent http param polution
+app.use(cors()); //enabled cors for all routes
 
 // static files in public folder
 app.use(express.static(path.join(__dirname, "public")));
