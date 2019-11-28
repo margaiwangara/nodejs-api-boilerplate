14cd4652eecbf1276dd5f987efaa58b6b740ac13
margaiwangara <margaiwangara@gmail.com>
2019-11-28 15:30:20 +0300

    connected to mysql, successful result

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const sql = require("mysql");
 
 // set debug to true to display db responses
 mongoose.set("debug", true);
@@ -13,8 +14,23 @@ mongoose
     useCreateIndex: true,
     keepAlive: true
   })
-  .then(conn => console.log(`MongoDB Connected: ${conn.connection.host}`))
+  .then(conn =>
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold)
+  )
   .catch(error => console.log(error));
+
+// SQL DB Configuration
+const connection = sql.createConnection({
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DB
+});
+
+connection.connect(error => {
+  if (error) console.log(error);
+  console.log("Connection to SQL DB Successfull".cyan.bold);
+});
 
 module.exports.Foo = require("./foo");
 module.exports.User = require("./users");
